3b7318f43a0cea9a346d886cf2b1a2a6888ec38d
Ginja <virginija.meilune@gmail.com>
2022-09-16 13:57:22 +1000

    Adding functionality preparations

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("Testing")
+const addBtns = document.querySelectorAll(".add-btn");
+const saveBtns = document.querySelectorAll(".save-btn");
+const addContainer = document.querySelectorAll(".add-container");
+const addItems = document.querySelectorAll(".add-new-item");
+
+//Lists
+const itemLists = document.querySelectorAll(".kanban-item-list");
+const backlogList = document.getElementById("backlog-list");
+const progressList = document.getElementById("progress-list");
+const completeList = document.getElementById("complete-list");
+const onHoldList = document.getElementById("on-hold-list");
+
+//Initialize Arrays
+let backlogListArray = [];
+let progressListArray = [];
+let completeListArray = [];
+let onHoldListArray = [];
